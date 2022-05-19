4736c9db11450f235b71cea491f753f5de8ce35a
Michael <michael.castlewellan@outlook.com>
2022-05-19 23:01:43 +0100

    Initial Commit

---
+++ First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Michael McAlarney on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
